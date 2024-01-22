0f9adef6fb15854b181d214276fada582a832465
Anastasiya Guryanova <gurianova.lan@mail.ru>
2024-01-22 12:48:27 +0300

    initial

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert("Брендан Эйхраз работал язык программирования JavaScript");
+alert("JavaScript был разработан за 10 дней");
+alert("JavaScript появился в 1996г.");
+alert("JavaScript - это не Java");
+alert("Спецификация языка JavaScript называется ECMAScript");
